234d42898b162a00850a13023c1ea2b7afcdc530
LesleyDamaceno <lesleycgdamaceno@gmail.com>
2025-05-22 02:16:59 -0300

    Atualiza arquivos do projeto

--- app.py
+++ app.py
@@ -18,7 +18,8 @@ def get_conexao():
         host=os.getenv("DB_HOST"),
         user=os.getenv("DB_USER"),
         password=os.getenv("DB_PASSWORD"),
-        database=os.getenv("DB_NAME")
+        database=os.getenv("DB_NAME"),
+		connection_timeout=5
     )
 
 # Rota: Login (GET e POST)
